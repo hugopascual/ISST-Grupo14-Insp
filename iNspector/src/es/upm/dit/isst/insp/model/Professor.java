fb26502dc66ed77b6a2ad394cfdf55a8f6207635
Jakub Piatek <kubapiatek2@gmail.com>
2020-04-26 17:55:44 +0200

    Actualización de archivos

--- iNspector/src/es/upm/dit/isst/insp/model/Professor.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.isst.insp.model;
-
-public class Professor {
-
-	public Professor() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- iNspector/src/es/upm/dit/isst/insp/model/TFG.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.isst.insp.model;
-
-public class TFG {
-
-	public TFG() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
